2614471b6f7a2b311fb33620df0c6c48d61b9fc8
Nucoco <yuuta11511997@gmail.com>
2021-05-13 23:47:45 +0900

    Nothing Special

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, useEffect, useState } from 'react';
+import styles from './App.module.css';
+import { db } from './firebase'
+import { FormControl, List, makeStyles, TextField } from '@material-ui/core'
+import { AddToPhotos } from '@material-ui/icons'
+import TaskItem from './TaskItem';
 
-function App() {
+const useStyles = makeStyles({
+  field: {
+    marginTop: 30,
+    marginBottom: 20,
+  },
+  list: {
+    margin: "auto",
+    width: "40%",
+  },
+});
+
+const App: FC = () => {
+  const [tasks, setTasks] = useState([{ id: '', title: '' }])
+  const [input, setInput] = useState('')
+  const classes = useStyles()
+
+  useEffect(() => {
+    //usSub receive a method to quit listening database.
+    //So, unSub() is a method as the return value from db.collection().onSnapshot()
+    const unSub = db.collection('tasks').onSnapshot(snapshot => {//onSnapshot() listens to Cloud FireStore
+      setTasks(
+        snapshot.docs.map(doc => ({ id: doc.id, title: doc.data().title }))
+      )
+    })
+    return () => unSub();// if not defined as an arrow function, rendering goes wrong.
+  }, [])
+
+  const newTask = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    db.collection('tasks').add({ title: input })
+    setInput('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app__root}>
+      <h1>Todo App by React/Firebase</h1>
+      <br />
+      <FormControl>
+        <TextField
+          className={classes.field}
+          InputLabelProps={{ shrink: true }}
+          label='New task ?'
+          value={input}
+          onChange={(event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => setInput(event.target.value)}
+        />
+      </FormControl>
+      <button className={styles.app__icon} disabled={!input} onClick={newTask}>
+        <AddToPhotos />
+      </button>
+
+      <List className={classes.list}>
+        {tasks.map(task => (
+          <TaskItem key={task.id} id={task.id} title={task.title} />
+        ))}
+
+      </List>
     </div>
   );
 }
---
+++ src/TaskItem.tsx
@@ -0,0 +1,40 @@
+import React, { FC, useState } from 'react'
+import styles from "./TaskItem.module.css"
+import { Grid, ListItem, TextField } from '@material-ui/core'
+import { DeleteOutlined, EditOutlined } from '@material-ui/icons'
+import { db } from './firebase'
+
+interface Props {
+   id: string
+   title: string
+}
+
+const TaskItem: FC<Props> = (props) => {
+   const [title, setTitle] = useState(props.title)
+
+   const editTask = () => {
+      db.collection('tasks').doc(props.id).set({ title: title }), { merge: true }
+   }
+
+   const deleteTask = () => {
+      db.collection('tasks').doc(props.id).delete()
+   }
+
+   return (
+      <ListItem>
+         <h2>{props.title}</h2>
+         <Grid container justify="flex-end">
+            <TextField
+               InputLabelProps={{ shrink: true }}
+               label="Edit task"
+               value={title}
+               onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)}
+            />
+         </Grid>
+         <button className={styles.taskitem__icon} onClick={editTask}><EditOutlined /></button>
+         <button className={styles.taskitem__icon} onClick={deleteTask}><DeleteOutlined /></button>
+      </ListItem>
+   )
+}
+
+export default TaskItem
